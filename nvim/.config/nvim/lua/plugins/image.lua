b85b4dde913c97ae574c01698f6418d78b91ce1b
Yulong <zylbing@qq.com>
2026-02-28 16:51:35 +0800

    修改image.nvim配置

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -1,55 +1,31 @@
 return {
-  "3rd/image.nvim",
-  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-  opts = {
-    backend = "sixel", -- or "ueberzug" or "sixel"
-    processor = "magick_cli", -- or "magick_rock"
-    integrations = {
-      markdown = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = true,
-        only_render_image_at_cursor_mode = "popup", -- or "inline"
-        floating_windows = false, -- if true, images will be rendered in floating markdown windows
-        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+  {
+    "3rd/image.nvim",
+    opts = {
+      backend = "sixel",
+      processor = "magick_rock", -- 使用 FFI 加速，避免 CLI 的卡顿和乱码
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = true, -- 光标移上去显示
+          only_render_image_at_cursor_mode = "popup",
+          floating_windows = true,
+          filetypes = { "markdown", "vimwiki" },
+        },
       },
-      asciidoc = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = false,
-        only_render_image_at_cursor_mode = "popup",
-        floating_windows = false,
-        filetypes = { "asciidoc", "adoc" },
-      },
-      neorg = {
-        enabled = true,
-        filetypes = { "norg" },
-      },
-      rst = {
-        enabled = true,
-      },
-      typst = {
-        enabled = true,
-        filetypes = { "typst" },
-      },
-      html = {
-        enabled = false,
-      },
-      css = {
-        enabled = false,
-      },
+      max_width = 100,
+      max_height_window_percentage = 40,
+      tmux_show_only_in_active_window = true,
+      window_overlap_clear_enabled = true,
+      editor_only_render_when_focused = false,
     },
-    max_width = nil,
-    max_height = nil,
-    max_width_window_percentage = nil,
-    max_height_window_percentage = 50,
-    scale_factor = 1.0,
-    window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-    editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-    tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = { enabled = false },
+    },
   },
 }
